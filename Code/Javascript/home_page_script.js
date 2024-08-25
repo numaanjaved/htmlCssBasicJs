2c98917f67078adaaf843be8e2059e1cfc60c1ec
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-08-25 10:47:41 +0500

    Landing page section completed

--- Code/Javascript/home_page_script.js
+++ Code/Javascript/home_page_script.js
@@ -71,3 +71,64 @@ lists_container.appendChild(hamburger_button);
 hamburger_button.appendChild(i_tag);
 
 //<<<<<<<                 Header Section End          >>>>>>
+
+//<<<<<<<                 Main Section start         >>>>>>
+
+let main = document.createElement("main");
+body.appendChild(main);
+
+//<<<<<<<                 Landing Page Section start         >>>>>>
+
+// creating Landing Page section (Landing page text and img content)
+
+let landing_page_section = document.createElement("section");
+landing_page_section.classList.add("landing_page_section");
+main.appendChild(landing_page_section);
+let landing_page_content_div = document.createElement("div");
+landing_page_content_div.classList.add("landing_page_content");
+landing_page_section.appendChild(landing_page_content_div);
+
+// Landing Page text content
+
+let landing_page_text_content = document.createElement("div");
+landing_page_text_content.classList.add("text_content");
+landing_page_content_div.appendChild(landing_page_text_content);
+
+let landing_page_slogan_text = document.createElement("h3");
+landing_page_slogan_text.textContent = "We are best and creative agency";
+let landing_page_main_heading = document.createElement("h1");
+landing_page_main_heading.textContent =
+  "We turn creative ideas into your business.";
+let landing_page_text = document.createElement("p");
+landing_page_text.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusa reiciendis voluptatem ratione doloribus`;
+landing_page_text.classList.add("landing_page_description");
+landing_page_slogan_text.classList.add("slogan_text");
+landing_page_main_heading.classList.add("main_heading");
+let landing_page_btn_container = document.createElement("div");
+landing_page_btn_container.classList.add("btns_container");
+let buttons_inner_text = ["Our Story", "Read More"];
+buttons_inner_text.forEach((btn) => {
+  let button = document.createElement("button");
+  button.classList.add("btn");
+  button.textContent = btn;
+  landing_page_btn_container.appendChild(button);
+});
+landing_page_text_content.appendChild(landing_page_slogan_text);
+landing_page_text_content.appendChild(landing_page_main_heading);
+landing_page_text_content.appendChild(landing_page_text);
+landing_page_text_content.appendChild(landing_page_btn_container);
+
+// Landing Page Image:
+
+let landing_page_img_container = document.createElement("div");
+landing_page_img_container.classList.add("img_content");
+let landing_page_img_content = document.createElement("img");
+landing_page_img_content.setAttribute(
+  "src",
+  "../../Project_Assets/Landing_Page_img.png"
+);
+landing_page_img_content.setAttribute("alt", "Business Man Image");
+landing_page_content_div.appendChild(landing_page_img_container);
+landing_page_img_container.appendChild(landing_page_img_content);
+
+//<<<<<<<                 Landing Page Section end         >>>>>>
