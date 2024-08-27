4172cbad4dcf3796624e5dcd976f1bc0b48d352d
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-08-27 23:12:21 +0500

    Added Js script Error Handling(now scripts will load asynchronously)

--- Assets/javascript/home_page.js
+++ Assets/javascript/home_page.js
@@ -1,24 +1,73 @@
 let body = document.body;
+
 // Defining Landing Page Container:
 // It is just for styling purpose
+
 let landing_page_container = document.createElement("div");
 landing_page_container.classList.add("landing_Page_container");
 
-let header_script = document.createElement("script");
-header_script.setAttribute("src", "./Assets/javascript/page_header.js");
-header_script.defer = true;
-document.head.appendChild(header_script);
+// Scripts Functions
+let scripts_func = async () => {
+  let header_script_func = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let header_script = document.createElement("script");
+      header_script.setAttribute("src", "./Assets/javascript/page_header.js");
+      header_script.defer = true;
+      document.head.appendChild(header_script);
+      header_script.onload = () => {
+        resolve("Header Script Loaded Successfully");
+      };
 
-let main_script = document.createElement("script");
-main_script.setAttribute("src", "./Assets/javascript/page_main.js");
-main_script.defer = true;
-document.head.appendChild(main_script);
+      header_script.onerror = () => {
+        reject("Error Occurred while Loading Header Script");
+      };
+    }, 10);
+  });
+  let main_script_func = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let main_script = document.createElement("script");
+      main_script.setAttribute("src", "./Assets/javascript/page_main.js");
+      main_script.defer = true;
+      document.head.appendChild(main_script);
 
-let footer_script = document.createElement("script");
-footer_script.setAttribute("src", "./Assets/javascript/page_footer.js");
-footer_script.defer = true;
-document.head.appendChild(footer_script);
+      main_script.onload = () => {
+        resolve("Main Script Loaded Successfully");
+      };
 
-// Task Completed version 5 Details:
-//  JS files divided in sections
-// folder structure changed
+      main_script.onerror = () => {
+        reject("Error Occurred while Loading Main Script");
+      };
+    }, 20);
+  });
+  let footer_script_func = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let footer_script = document.createElement("script");
+      footer_script.setAttribute("src", "./Assets/javascript/page_footer.js");
+      footer_script.defer = true;
+      document.head.appendChild(footer_script);
+
+      footer_script.onload = () => {
+        resolve("Footer Script Loaded Successfully");
+      };
+
+      footer_script.onerror = () => {
+        reject("Error Occurred while Loading Footer Script");
+      };
+    }, 3000);
+  });
+
+  let async_header = await header_script_func;
+  let async_main = await main_script_func;
+  let async_footer = await footer_script_func;
+  return [async_header, async_main, async_footer];
+};
+
+let run_func = scripts_func();
+
+run_func
+  .then((value) => {
+    value.forEach((val) => console.log(val));
+  })
+  .catch((err) => {
+    console.error(err);
+  });
