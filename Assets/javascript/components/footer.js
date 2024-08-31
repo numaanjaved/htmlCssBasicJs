0b9e0c9e7722bd4056ec9d87216ff39b5adc4e85
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-08-31 11:35:08 +0500

    Footer code minimized and data objects are created separately

--- Assets/javascript/components/footer.js
+++ Assets/javascript/components/footer.js
@@ -1,9 +1,7 @@
 //<<<<<<<====                 Footer Section start             ====>>>>>
 
 createNewElement("footer", "footer", body);
-
 createNewElement("section", "footer_section_content", ".footer");
-
 createNewElement("div", "main_content", ".footer_section_content");
 createNewElement("div", "text_content_div", ".main_content");
 
@@ -16,38 +14,19 @@ let links_in_company = [
 ];
 links_in_company.forEach((links_names, index) => {
   let subDiv = createNewElement("div", "text_sub_content", ".text_content_div");
-  createNewElement(
-    "h4",
-    "section_sub_heading",
-    subDiv,
-    `${name_of_companies[index]}`
-  );
-
+  createNewElement("h4", "section_sub_heading", subDiv,`${name_of_companies[index]}`);
   //Adding company links list:
   let company_linkList = createNewElement("ul", "company_list", subDiv);
-
   //   Creating li and extracting data from Array
   links_names.forEach((li_link_name) => {
-    let li_items = createNewElement(
-      "li",
-      "company_list_items",
-      company_linkList
-    );
-
+    let li_items = createNewElement("li", "company_list_items", company_linkList);
     //   Now creating Link of Li:
-
-    let company_li_link = createNewElement(
-      "a",
-      "company_item_link",
-      li_items,
-      `${li_link_name}`,
-      { href: "#" }
-    );
+    let company_links_obj={ href: "#" };
+    let company_li_link = createNewElement( "a", "company_item_link", li_items, `${li_link_name}`,company_links_obj);
   });
 });
 
 createNewElement("div", "img_content_div", ".main_content");
-
 let company_img_container = ["div1", "div2", "div3", "div4"];
 let company_images_data = [
   { src: "./Assets/images/Company_img1.webp", alt: "Company Image" },
@@ -57,50 +36,32 @@ let company_images_data = [
 ];
 company_img_container.forEach((div_data, index) => {
   let div = createNewElement("div", "company_image", ".img_content_div");
-
+let company_img_obj={
+  src: company_images_data[index].src,
+  alt: company_images_data[index].alt,
+}
   //   creating_img tag:
-  let company_img = createNewElement("img", null, div, null, {
-    src: company_images_data[index].src,
-    alt: company_images_data[index].alt,
-  });
+  let company_img = createNewElement("img", null, div, null,company_img_obj );
 });
 
 // bottom part of Footer:
 
 createNewElement("div", "sub_content", ".footer_section_content");
-
 createNewElement("div", "text_content_footer_bottom", ".sub_content");
-
 createNewElement("ul", "footer_items", ".text_content_footer_bottom");
 
 let footer_li_items_array = ["li1", "li2", "li3", "li4"];
 
-let Array_of_li_names = [
-  `\u00A9 Themes by psdfreebies.com`,
-  "Privacy",
-  "Terms of Use",
-  "Site Map",
-];
+let Array_of_li_names = [ `\u00A9 Themes by psdfreebies.com`, "Privacy", "Terms of Use", "Site Map", ];
 Array_of_li_names.forEach((list_name, index) => {
-  let bottom_footer_list_item = createNewElement(
-    "li",
-    "footer_list_item",
-    ".footer_items"
-  );
-
+  let bottom_footer_list_item = createNewElement("li", "footer_list_item",".footer_items");
   //   now adding links in it:
   if (index == 0) {
-    createNewElement("span", null, bottom_footer_list_item, `${list_name}`, {
-      id: "copyright",
-    });
-  } else if (index != 0) {
-    let footer_list_item_link = createNewElement(
-      "a",
-      "footer_list_item_link",
-      bottom_footer_list_item,
-      `${list_name}`,
-      { href: "#" }
-    );
+    let copyright_text_obj={id: "copyright"};
+    createNewElement("span", null, bottom_footer_list_item, `${list_name}`,copyright_text_obj);
+  } else {
+   let footer_links_href_obj={ href: "#" };
+    let footer_list_item_link = createNewElement("a","footer_list_item_link", bottom_footer_list_item, `${list_name}`,footer_links_href_obj);
   }
 });
 
