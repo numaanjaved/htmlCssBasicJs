81a36f31c841d53a9a0a07bd097504f8a8b0bb65
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-08-31 11:53:42 +0500

    Header code minimized and data objects are created separately

--- Assets/javascript/components/header.js
+++ Assets/javascript/components/header.js
@@ -1,28 +1,15 @@
-let createNewElement = (
-  newElement,
-  elementClass = null,
-  elementContainer,
-  elementContent = null,
-  attributes = {}
-) => {
+let createNewElement = ( newElement, elementClass = null, elementContainer, elementContent = null,attributes = {}) => {
   let htmlElement = document.createElement(newElement);
   if (elementClass) {
-    // console.log(`Adding classes: ${elementClass}`);
     if (Array.isArray(elementClass)) {
-      elementClass.forEach((cls) => {
-        htmlElement.classList.add(cls);
-      });
-    } else {
-      htmlElement.classList.add(elementClass);
-    }
+      elementClass.forEach((cls) => { htmlElement.classList.add(cls);});
+    } else { htmlElement.classList.add(elementClass);}
   }
   if (elementContent != null) {
-    htmlElement.innerHTML = elementContent;
-  }
+    htmlElement.innerHTML = elementContent;}
   if (attributes) {
     for (let attr_key in attributes) {
-      htmlElement.setAttribute(attr_key, attributes[attr_key]);
-    }
+      htmlElement.setAttribute(attr_key, attributes[attr_key]);}
   }
   if (typeof elementContainer === "string") {
     let selectParent = document.querySelector(elementContainer);
@@ -43,17 +30,13 @@ createNewElement("div", "landing_Page_container", body);
 //<<<<<<<                 Header Section start          >>>>>>
 
 createNewElement("header", "header", body);
-
 createNewElement("nav", "navbar", ".header");
 
 // logo div:
 createNewElement("div", "logo", ".navbar");
-createNewElement("img", "rexlogo", ".logo", null, {
-  src: "./Assets/images/BlueRex_Logo.webp",
-  alt: "RexLogo Icon",
-});
+let rex_logo_img_obj= { src: "./Assets/images/BlueRex_Logo.webp", alt: "RexLogo Icon" }
+createNewElement("img", "rexlogo", ".logo", null,rex_logo_img_obj );
 createNewElement("span", "rexlogo_text", ".logo", "BlueRex");
-
 // Navbar Lists
 createNewElement("div", "list_container", ".navbar");
 let nav_ul = createNewElement("ul", "header_items_list", ".list_container");
@@ -69,20 +52,15 @@ let array_of_nav_links = [
   { href: "/", text: "Faq" },
   { href: "/", text: "Contact" },
 ];
-
 array_of_nav_links.forEach((li_items) => {
   // defined variable to store data in one variable and then put data in that variable for each iteration
-
   let li = createNewElement("li", "list_items", ".header_items_list");
-  createNewElement("a", "li_links", li, `${li_items.text}`, {
-    href: `${li_items.href}`,
-  });
+  let navbar_links_href_obj = {href: `${li_items.href}`};
+  createNewElement("a", "li_links", li, `${li_items.text}`, navbar_links_href_obj);
 });
-
 // navbar Hamburger Button:
 let hamburger_button = createNewElement("button", "btn", ".list_container");
 createNewElement("span", "bars", hamburger_button);
-
 // hamburger_functionality=>:
 hamburger_button.addEventListener("click", (e) => {
   if (nav_ul.classList.contains("flex") == true) {
