edc6f3942ffe7c4b0d38e0d572b2031034f7b7b1
MuhammadAbdullahg <jdifn74@gmail.com>
2025-09-04 12:21:42 +0500

    23-text-to-speech-app

---
+++ 23-text-to-speech-app/script.js
@@ -0,0 +1,65 @@
+const synth = window.speechSynthesis;
+
+const textForm = document.querySelector("form");
+const textInput = document.querySelector("#text-input");
+const voiceSelect = document.querySelector("#voice-select");
+const rate = document.querySelector("#rate");
+const rateValue = document.querySelector("#rate-value");
+const pitch = document.querySelector("#pitch");
+const pitchValue = document.querySelector("#pitch-value");
+const body = document.querySelector("body");
+
+let voices = [];
+
+const getVoices = () => {
+  voices = synth.getVoices();
+  voices.forEach((voice) => {
+    const option = document.createElement("option");
+    option.setAttribute("data-name", voice.name);
+    option.setAttribute("data-lang", voice.lang);
+    option.textContent = voice.name + "(" + voice.lang + ")";
+    voiceSelect.appendChild(option);
+  });
+};
+
+if (synth.onvoiceschanged !== undefined) {
+  synth.onvoiceschanged = getVoices;
+}
+
+const speak = () => {
+  if (synth.speaking) {
+    body.style.background = "#141414 url(img/wave.gif)";
+    body.style.backgroundRepeat = "repeat-x";
+    body.style.backgroundSize = "100% 100%";
+    console.log("Already speaking");
+  }
+  if (textInput.value !== "") {
+    const speakText = new SpeechSynthesisUtterance(textInput.value);
+    speakText.onend = (e) => {
+      body.style.background = "#141414";
+      console.log("Speaking done");
+    };
+    const selectedVoice =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
+    voices.forEach((voice) => {
+      if (voice.name === selectedVoice) {
+        speakText.voice = voice;
+      }
+    });
+    speakText.rate = rate.value;
+    speakText.pitch = pitch.value;
+    synth.speak(speakText);
+  }
+};
+textForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  speak();
+  textInput.blur();
+});
+rate.addEventListener("change", (e) => {
+  rateValue.textContent = rate.value;
+});
+pitch.addEventListener("change", (e) => {
+  pitchValue.textContent = pitch.value;
+});
+voiceSelect.addEventListener("change", (e) => speak());
