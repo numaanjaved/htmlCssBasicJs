d81fe3b9ca1f459bbffced0dfb4255f99199b658
MuhammadAbdullahg <jdifn74@gmail.com>
2025-09-11 12:22:19 +0500

    33-coming-soon-landing-page

---
+++ 33-coming-soon-landing-page/script.js
@@ -0,0 +1,18 @@
+const countDown = document.querySelector(".countDown");
+const lanuchDate = new Date("Jan 1,2026 13:00:00").getTime();
+let intvl = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = lanuchDate - now;
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  countDown.innerHTML = `
+  <div>${days}<span>Days</span></div>
+  <div>${hours}<span>Hours</span></div>
+  <div>${mins}<span>Mins</span></div>
+  <div>${seconds}<span>Seconds</span></div>
+  `;
+}, 1000);
