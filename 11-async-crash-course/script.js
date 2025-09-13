a8109963e47aacf214223574e084ad782a266143
MuhammadAbdullahg <jdifn74@gmail.com>
2025-09-13 08:49:07 +0500

    11-async-crash-course

--- 11-async-crash-course/script.js
+++ 11-async-crash-course/script.js
@@ -1,28 +1,31 @@
-// const posts = [
-//   { title: "Post one", body: "this is post one" },
-//   { title: "Post two", body: "this is post two" },
-// ];
+// create array of objects
 
-// function getPosts() {
-//   setTimeout(() => {
-//     let output = "";
-//     posts.forEach((post, index) => {
-//       output += `<li>${post.title}</li>`;
-//       document.body.innerHTML = output;
-//     });
-//   }, 1000);
-// }
+const posts = [
+  { title: "Post one", body: "this is post one" },
+  { title: "Post two", body: "this is post two" },
+];
 
-// function createPost(post, callback) {
-//   setTimeout(() => {
-//     posts.push(post);
-//     callback();
-//   }, 2000);
-// }
+// create function and display output on document and set time like server response
+function getPosts() {
+  setTimeout(() => {
+    let output = "";
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;
+      document.body.innerHTML = output;
+    });
+  }, 1000);
+}
+// using callbacks older way 
+function createPost(post, callback) {
+  setTimeout(() => {
+    posts.push(post);
+    callback();
+  }, 2000);
+}
 
 // promise
 
-const posts = [
+const otherPost = [
   { title: "Post one", body: "this is post one" },
   { title: "Post two", body: "this is post two" },
 ];
@@ -36,7 +39,7 @@ function getPosts() {
     });
   }, 1000);
 }
-
+// modern way than call backs if resolve return data other wise error
 function createPost(post) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -53,12 +56,12 @@ function createPost(post) {
 }
 
 // async / await
-
-// async function hello() {
-//   await createPost({ title: "Book three", body: "this is book three" });
-//   getPosts();
-// }
-// hello();
+// cleaner way than than promise it automatically return ture and other processing stop until await is not finished
+async function hello() {
+  await createPost({ title: "Book three", body: "this is book three" });
+  getPosts();
+}
+hello();
 
 // async / await / fetch
 
@@ -70,16 +73,16 @@ async function helloUser() {
 helloUser()
 
 // promise.all
+// promise all to get result of all romises if it is true if one promise is failed all promise not work
+const promise1 = Promise.resolve("hello world");
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 2000, "goodbye");
+});
+const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(
+  (res) => res.json()
+);
 
-// const promise1 = Promise.resolve("hello world");
-// const promise2 = 10;
-// const promise3 = new Promise((resolve, reject) => {
-//   setTimeout(resolve, 2000, "goodbye");
-// });
-// const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(
-//   (res) => res.json()
-// );
-
-// Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
-//   console.log(values)
-// );
+Promise.all([promise1, promise2, promise3, promise4]).then((values) =>
+  console.log(values)
+);
