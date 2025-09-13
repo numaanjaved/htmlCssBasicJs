fc3c3886819da3603616cc8025d9c1c9fd3dfa47
MuhammadAbdullahg <jdifn74@gmail.com>
2025-09-13 10:11:44 +0500

    22-number-fetch-app

--- 22-number-fetch-app/script.js
+++ 22-number-fetch-app/script.js
@@ -1,27 +1,30 @@
 let numberInput = document.querySelector("#numFacts");
 let factDiv = document.querySelector(".facts");
 let outputFact = document.querySelector(".fact");
-numberInput.addEventListener("input", getFactfetch);
-// function getFactajax() {
-//   let number = numberInput.value;
-//   let xhr = new XMLHttpRequest();
-//   xhr.open("GET", "http://numbersapi.com/" + number);
-//   xhr.onload = function () {
-//     if (this.status == 200) {
-//       factDiv.style.display = "block";
-//       outputFact.innerHTML = this.responseText;
-//     }
-//   };
-//   xhr.send();
-// }
-
-function getFactfetch() {
+numberInput.addEventListener("input", getFactajax);
+function getFactajax() {
   let number = numberInput.value;
-  fetch("http://numbersapi.com/" + number)
-    .then((response) => response.text())
-    .then((data) => {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://numbersapi.com/" + number);
+  xhr.onload = function () {
+    if (this.status == 200) {
       factDiv.style.display = "block";
-      outputFact.innerHTML = data;
-    })
-    .catch((err) => console.log(err));
+      outputFact.innerHTML = this.responseText;
+    }
+    if (number === "") {
+      outputFact.innerHTML = "";
+    }
+  };
+  xhr.send();
 }
+
+// function getFactfetch() {
+//   let number = numberInput.value;
+//   fetch("http://numbersapi.com/" + number)
+//     .then((response) => response.text())
+//     .then((data) => {
+//       factDiv.style.display = "block";
+//       outputFact.innerHTML = data;
+//     })
+//     .catch((err) => console.log(err));
+// }
