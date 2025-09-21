e54c932be74a6f051b0c0b5990bcd09658666212
MuhammadAbdullahg <jdifn74@gmail.com>
2025-09-21 10:20:26 +0500

    39-dynamic-landing-age

--- 39-dynamic-landing-age/script.js
+++ 39-dynamic-landing-age/script.js
@@ -1,75 +1,104 @@
 // DOM Element
-const time = document.querySelector("#time"),
+let time = document.querySelector("#time"),
   greeting = document.querySelector("#greeting"),
   name = document.querySelector("#name"),
   message = document.querySelector("#message");
 
 // show time
 function showTime() {
+  // get time
   let today = new Date(),
     hour = today.getHours(),
-    min = today.getMinutes(),
+    mins = today.getMinutes(),
     sec = today.getSeconds();
-  // set AM or PM
-  const ampm = hour >= 12 ? "AM" : "PM";
-  // 12 hour format
-  hour = hour % 12 || 12;
-  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
-    sec
-  )}`;
+  // set time in DOM
+  time.innerHTML = `${hour}<span>:</span>${addZero(
+    mins
+  )}<span>:</span>${addZero(sec)}`;
+  // update time after second
   setTimeout(showTime, 1000);
+  //   set 12 hour format
+  hour = hour % 12 || 12;
 }
-// addzeroe
+
+// add zero
 function addZero(n) {
   return (parseInt(n, 10) < 10 ? "0" : "") + n;
 }
 
+// set wallpaper and greeting according to time
 function setBgGreet() {
+  // get time
   let today = new Date(),
     hour = today.getHours();
+  // check time then update
   if (hour < 12) {
-    // document.body.style.background =
-    //   "url('./img/morning.jpg') no-repeat center center/cover";
-    greeting.innerHTML = "Good Morning";
+    document.body.style.background =
+      "url(./img/morning.jpg) no-repeat center center/cover";
+    greeting.textContent = `Good Morning`;
   } else if (hour < 18) {
-    // document.body.style.background =
-    //   "url('./img/afternoon.jpg') no-repeat center center/cover";
-    greeting.innerHTML = "Good Afternoon";
-  }
-  // document.body.style.background =
-  //   "url('./img/evening.jpg') no-repeat center center/cover";
-  else greeting.innerHTML = "Good Evening";
-}
-
-// get name
-function getName() {
-  if (localStorage.getItem("name") === null) {
-    name.textContent = "[Enter name]";
+    document.body.style.background =
+      "url(./img/afternoon.jpg) no-repeat center center/cover";
+    greeting.textContent = `Good Afternoon`;
   } else {
-    name.textContent = "";
+    document.body.style.background =
+      "url(./img/night.jpg) no-repeat center center/cover";
+    greeting.textContent = `Good Night`;
   }
 }
 
-// set name
-name.addEventListner("keypress", setName);
-
-function setName(e) {
-  if (e.type === "keypress") {
-    if (e.leyCode == 13) {
-      localStorage.setItem("name", name.innerHTML);
-      name.blur();
-    }
+// local storage save name and message functions
+function getName() {
+  // check if local is null and update
+  if (localStorage.getItem("name") === null) {
+    // if null the set name for enter
+    localStorage.getItem("name", (name.textContent = "[Enter Name]"));
+  } else {
+    name.textContent = localStorage.getItem("name");
   }
 }
 
 function getMessage() {
+  // check if local is null and update
   if (localStorage.getItem("message") === null) {
-    message.textContent = "[Enter message]";
+    // if null the set name for enter
+    localStorage.getItem("message", (message.textContent = "[Enter Message]"));
   } else {
-    message.textContent = "";
+    message.textContent = localStorage.getItem("message");
   }
 }
 
+function setName(e) {
+  // if key press
+  if (e.type == "keypress") {
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("name", e.target.innerText);
+      name.blur();
+    } else {
+      localStorage.setItem("name", e.target.innerText);
+    }
+  }
+}
+
+function setMessage(e) {
+  // if key press
+  if (e.type == "keypress") {
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("message", e.target.innerText);
+      message.blur();
+    } else {
+      localStorage.setItem("message", e.target.innerText);
+    }
+  }
+}
+
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+
+message.addEventListener("keypress", setMessage);
+message.addEventListener("blur", setMessage);
+
+// run
 showTime();
 setBgGreet();
 getName();
