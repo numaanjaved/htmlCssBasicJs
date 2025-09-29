b42ffc2f536a467d57f85db5d616c07d1bb549d0
MuhammadAbdullahg <jdifn74@gmail.com>
2025-09-29 14:47:17 +0500

    16-bookmarker-app

--- 16-bookmarker-app/script.js
+++ 16-bookmarker-app/script.js
@@ -16,10 +16,7 @@ function saveBookMark(e) {
     return false;
   }
   document.querySelector("#myForm").reset();
-  let bookMark = {
-    name: siteName,
-    url: siteUrl,
-  };
+  let bookMark = { name: siteName, url: siteUrl };
   if (localStorage.getItem("bookmarks") === null) {
     let bookMarks = [];
     bookMarks.push(bookMark);
@@ -31,30 +28,34 @@ function saveBookMark(e) {
   }
   fetchResult();
 }
-
 function fetchResult() {
   var booksSaved = JSON.parse(localStorage.getItem("bookmarks"));
   let bookmarkresult = document.querySelector("#output");
-  bookmarkresult.innerHTML = "";
   for (var i = 0; i < booksSaved.length; i++) {
     var name = booksSaved[i].name;
     var url = booksSaved[i].url;
-    bookmarkresult.innerHTML +=
-      '<div class="bookOutput">' +
-      name +
-      "<h3>" +
-      '<a class="btn" target="_blank" href="' +
-      url +
-      '">visit</a>' +
-      "<a onClick=\"deletebookmark('" +
-      url +
-      '\')" class="btn" href="#">delete</a>' +
-      "</h3>" +
-      "</div>";
+    let outputDiv = document.createElement("div");
+    bookmarkresult.appendChild(outputDiv);
+    outputDiv.innerText = name;
+    outputDiv.className = "bookOutput";
+    let h3 = document.createElement("h3");
+    outputDiv.appendChild(h3);
+    let a = document.createElement("a");
+    a.className = "btn";
+    a.href = url;
+    a.target = "_blank";
+    a.textContent = "Visit";
+    h3.append(a);
+    let a2 = document.createElement("a");
+    a2.className = "btn btn-delete";
+    a2.textContent = "Delete";
+    h3.append(a2);
+    a2.addEventListener("click", function (event) {
+      deletebookmark(url, event);
+    });
   }
 }
-
-function deletebookmark(url) {
+function deletebookmark(url, event) {
   var olderbooksaved = JSON.parse(localStorage.getItem("bookmarks"));
   for (var i = 0; i < olderbooksaved.length; i++) {
     if (olderbooksaved[i].url == url) {
@@ -62,5 +63,9 @@ function deletebookmark(url) {
     }
   }
   localStorage.setItem("bookmarks", JSON.stringify(olderbooksaved));
+  const bookDiv = event.target.closest(".bookOutput");
+  if (bookDiv) {
+    bookDiv.remove();
+  }
   fetchResult();
 }
