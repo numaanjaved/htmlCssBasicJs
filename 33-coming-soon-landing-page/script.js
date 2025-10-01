cc5b3f930eeb3e11d69d11622010202a4947bee6
MuhammadAbdullahg <jdifn74@gmail.com>
2025-10-01 12:24:52 +0500

    33-coming-soon-landing-page

--- 33-coming-soon-landing-page/script.js
+++ 33-coming-soon-landing-page/script.js
@@ -9,15 +9,21 @@ let intvl = setInterval(() => {
   );
   const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  divD.innerHTML = `${days}`;
-  divD.appendChild(spanD);
-  divH.innerHTML = `${hours}`;
-  divH.appendChild(spanH);
-  divM.innerHTML = `${mins}`;
-  divM.appendChild(spanM);
-  divS.innerHTML = `${seconds}`;
-  divS.appendChild(spanS);
-  countDown.append(divD, divH, divM, divS);
+  let timeArr = [
+    { divText: `${days}`, spanText: "Days" },
+    { divText: `${hours}`, spanText: "Hours" },
+    { divText: `${mins}`, spanText: "Mins" },
+    { divText: `${seconds}`, spanText: "Seconds" },
+  ];
+  countDown.innerHTML = "";
+  timeArr.forEach((time) => {
+    let div = document.createElement("div");
+    let span = document.createElement("span");
+    div.textContent = time.divText;
+    span.textContent = time.spanText;
+    div.appendChild(span);
+    countDown.append(div);
+  });
   if (distance < 0) {
     clearInterval(intvl);
     countDown.style.color = "#587543";
@@ -26,19 +32,3 @@ let intvl = setInterval(() => {
     countDown.appendChild(div);
   }
 }, 1000);
-
-let divD = document.createElement("div");
-let spanD = document.createElement("span");
-spanD.innerText = "Days";
-
-let divH = document.createElement("div");
-let spanH = document.createElement("span");
-spanH.innerText = "Hours";
-
-let divM = document.createElement("div");
-let spanM = document.createElement("span");
-spanM.innerText = "Mins";
-
-let divS = document.createElement("div");
-let spanS = document.createElement("span");
-spanS.innerText = "Seconds";
