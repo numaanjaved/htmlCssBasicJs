bd6cd4d5daa3509e69d5a758a6ec8461f5c861ac
MuhammadAbdullahg <jdifn74@gmail.com>
2025-10-30 16:55:58 +0500

    58-tesla-car-configration

--- 58-tesla-car-configration/js/main.js
+++ 58-tesla-car-configration/js/main.js
@@ -5,11 +5,87 @@ const exteriorImage = document.querySelector("#exterior-image");
 const interiorImage = document.querySelector("#interior-image");
 const wheelButtonSection = document.querySelector("#wheel-buttons");
 const performanceBtn = document.querySelector("#performance-btn");
+const totalPriceElement = document.querySelector("#total-price");
+const fullSelfDrivingCheckBox = document.querySelector(
+  "#full-self-driving-checkbox"
+);
+const accessoryCheckBoxes = document.querySelectorAll(
+  ".accessory-form-checkbox"
+);
+const downPaymentElement = document.querySelector("#down-payment");
+const monthlyPaymentElement = document.querySelector("#monthly-payment");
+
+const basePrice = 52490;
+let currentPrice = basePrice;
+
 let selectedColor = "Stealth Grey";
 const selectedOptions = {
-  "performance wheels": false,
-  "performance package": false,
-  "full self-driving": false,
+  "Performance Wheels": false,
+  "Performance Package": false,
+  "Full Self-Driving": false,
+};
+
+const pricing = {
+  "Performance Wheels": 2500,
+  "Performance Package": 5000,
+  "Full Self-Driving": 8500,
+  Accessories: {
+    "Center Console Trays": 35,
+    Sunshade: 105,
+    "All-Weather Interior Liners": 225,
+  },
+};
+
+// handle price
+const updateTotalPrice = () => {
+  currentPrice = basePrice;
+
+  if (selectedOptions["Performance Wheels"]) {
+    currentPrice += pricing["Performance Wheels"];
+  }
+
+  if (selectedOptions["Performance Package"]) {
+    currentPrice += pricing["Performance Package"];
+  }
+
+  if (selectedOptions["Full Self-Driving"]) {
+    currentPrice += pricing["Full Self-Driving"];
+  }
+
+  accessoryCheckBoxes.forEach((checkbox) => {
+    const accessoryLabel = checkbox
+      .closest("label")
+      .querySelector("span")
+      .textContent.trim();
+    const accessoryOption = pricing.Accessories[accessoryLabel];
+
+    if (checkbox.checked) {
+      currentPrice += accessoryOption;
+    }
+  });
+  totalPriceElement.textContent = `$${currentPrice.toLocaleString()}`;
+  updatePrice();
+};
+
+const updatePrice = () => {
+  const downPayment = currentPrice * 0.1;
+  downPaymentElement.textContent = `$${downPayment.toLocaleString()}`;
+
+  // calculate loan detail
+  const loanTermMonth = 60;
+  const interestRate = 0.03;
+
+  const loanAmount = currentPrice - downPayment;
+
+  const monthlyInterestRate = interestRate / 2;
+  const monthlyPayment =
+    (loanAmount *
+      (monthlyInterestRate *
+        Math.pow(1 + monthlyInterestRate, loanTermMonth))) /
+    (Math.pow(1 + monthlyInterestRate, loanTermMonth) - 1);
+  monthlyPaymentElement.textContent = `$${monthlyPayment
+    .toFixed(2)
+    .toLocaleString()}`;
 };
 
 // handles top bar
@@ -62,7 +138,7 @@ const colorhandle = (event) => {
 };
 // exterior image and performance wheel
 const updateExteriorImage = () => {
-  const performanceSuffix = selectedOptions["performance wheels"]
+  const performanceSuffix = selectedOptions["Performance Package"]
     ? "-performance"
     : "";
   const colorKey =
@@ -80,16 +156,31 @@ const wheelHandle = (event) => {
     buttons.forEach((btn) => btn.classList.remove("bg-gray-700", "text-white"));
     event.target.classList.add("bg-gray-700", "text-white");
 
-    selectedOptions["performance wheels"] =
+    selectedOptions["Performance Package"] =
       event.target.textContent.includes("Performance");
     updateExteriorImage();
+    updateTotalPrice();
   }
 };
 // handle performance
 const handlePerformance = () => {
-  performanceBtn.classList.toggle("bg-gray-700");
+  const isSelected = performanceBtn.classList.toggle("bg-gray-700");
   performanceBtn.classList.toggle("text-white");
+
+  selectedOptions["Performance Package"] = isSelected;
+  updateTotalPrice();
 };
+
+// full self driving change
+const fullSeflDrivingChange = () => {
+  selectedOptions["Full Self-Driving"] = fullSelfDrivingCheckBox.checked;
+  updateTotalPrice();
+};
+
+// accessory checkbox
+accessoryCheckBoxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", () => updateTotalPrice());
+});
 
 // event listeners
 window.addEventListener("scroll", () => requestAnimationFrame(scrollHandle));
@@ -97,3 +188,4 @@ exteriorColorSection.addEventListener("click", colorhandle);
 interiorColorSection.addEventListener("click", colorhandle);
 wheelButtonSection.addEventListener("click", wheelHandle);
 performanceBtn.addEventListener("click", handlePerformance);
+fullSelfDrivingCheckBox.addEventListener("change", fullSeflDrivingChange);
