998611dcd884b0c6751b6340a37a4e63d7c5ad8e
MuhammadAbdullahg <jdifn74@gmail.com>
2025-11-15 21:05:18 +0500

    javascript-login

--- modal.js
+++ modal.js
@@ -19,23 +19,13 @@ export function localStorageData(list) {
   }
 }
 
-let data = JSON.parse(localStorage.getItem("user"));
-
-if (data == null) {
-  signInEmail.value = "";
-  signInPassword.value = "";
-  makeTag(
-    "incorrectT",
-    "incorrectToast util",
-    "Please enter valid email and password",
-    signInDiv,
-    signInButton
-  );
-}
 export function signInCheck() {
+  let data = JSON.parse(localStorage.getItem("user"));
+  console.log(data);
   let signInEmailValue = signInEmail.value;
   let signInPasswordValue = signInPassword.value;
 
+  window.addEventListener("DOMContentLoaded", signInCheck);
   data.forEach((user, index) => {
     const userEmail = user.email;
     const userPassword = user.password;
